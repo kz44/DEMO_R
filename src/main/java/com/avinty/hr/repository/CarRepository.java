fe019f33d43ec9669e03595b6ba4d22f0d333a26
Kollonay Zoltán <zoltan@kollonay.hu>
2025-01-20 14:05:38 +0100

    feat: added custom query to car repository to find car by license plate

--- src/main/java/com/avinty/hr/repository/CarRepository.java
+++ src/main/java/com/avinty/hr/repository/CarRepository.java
@@ -2,6 +2,17 @@ package com.avinty.hr.repository;
 
 import com.avinty.hr.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+  @Query("SELECT c FROM Car c ORDER BY c.brand ASC")
+  List<Car> findALlCarsSortedByBrandASC();
+
+  @Query("SELECT c FROM Car c " +
+      "WHERE LOWER(c.licensePlate) LIKE LOWER(CONCAT('%', :licensePlate, '%')) " +
+      "ORDER BY c.brand ASC")
+  List<Car> findByLicensePlateContainingIgnoreCase(String licensePlate);
 }
