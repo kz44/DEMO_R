bd2a8a7b55ef067dcc0bed548b5b556c09220a04
Kollonay Zoltán <zoltan@kollonay.hu>
2025-01-20 14:00:49 +0100

    feat: added business logic for get all cars and ASC

--- src/main/java/com/avinty/hr/service/CarService.java
+++ src/main/java/com/avinty/hr/service/CarService.java
@@ -1,5 +1,10 @@
 package com.avinty.hr.service;
 
+import com.avinty.hr.DTO.CarDTO;
+
+import java.util.List;
+
 public interface CarService {
 
+  List<CarDTO> getAllCars();
 }
--- src/main/java/com/avinty/hr/service/CarServiceImp.java
+++ src/main/java/com/avinty/hr/service/CarServiceImp.java
@@ -1,13 +1,26 @@
 package com.avinty.hr.service;
 
+import com.avinty.hr.DTO.CarDTO;
+import com.avinty.hr.mapper.CarMapper;
 import com.avinty.hr.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CarServiceImp implements CarService {
 
   private final CarRepository carRepository;
+  private final CarMapper carMapper;
 
+  @Override
+  public List<CarDTO> getAllCars() {
+    return carRepository.findALlCarsSortedByBrandASC()
+        .stream()
+        .map(carMapper::toDTO)
+        .collect(Collectors.toList());
+  }
 }
