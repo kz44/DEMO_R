0b80434ca7e91f15aac4ad8f69e498b779b18e0c
Kollonay Zoltán <zoltan@kollonay.hu>
2025-01-20 14:21:04 +0100

    feat: added javadoc to car repository

--- src/main/java/com/avinty/hr/repository/CarRepository.java
+++ src/main/java/com/avinty/hr/repository/CarRepository.java
@@ -8,13 +8,33 @@ import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+  /**
+   * Returns all cars ordered by brand in ascending order.
+   *
+   * @return List of cars.
+   */
   @Query("SELECT c FROM Car c ORDER BY c.brand ASC")
   List<Car> findALlCarsSortedByBrandASC();
 
+
+  /**
+   * Finds cars whose license plate contains the given string, case-insensitive, ordered by brand in ascending order.
+   *
+   * @param licensePlate the part of the license plate to search for.
+   * @return a list of matching cars.
+   */
   @Query("SELECT c FROM Car c " +
       "WHERE LOWER(c.licensePlate) LIKE LOWER(CONCAT('%', :licensePlate, '%')) " +
       "ORDER BY c.brand ASC")
   List<Car> findByLicensePlateContainingIgnoreCase(String licensePlate);
 
+
+
+  /**
+   * Checks if a car with the given license plate exists, case-insensitive.
+   *
+   * @param licensePlate the license plate to check.
+   * @return true if car exists, false otherwise.
+   */
   boolean existsCarByLicensePlateIgnoreCase(String licensePlate);
 }
